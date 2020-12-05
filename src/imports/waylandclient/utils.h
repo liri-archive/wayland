liri-archive/wayland

commit 94fdc82b961185ec1f1536b896713b8f54997301

Add children
Add `data` property so that we can add children.

src/imports/waylandclient/utils.h
@@ -1,0 +1,23 @@
+// SPDX-FileCopyrightText: 2020 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#ifndef LIRI_WAYLANDCLIENT_QML_UTILS_H
+#define LIRI_WAYLANDCLIENT_QML_UTILS_H
+
+#define LIRI_DECLARE_CONTAINER_CLASS(className) \
+    class className##Container : public className \
+    { \
+        Q_OBJECT \
+        Q_PROPERTY(QQmlListProperty<QObject> data READ data DESIGNABLE false) \
+        Q_CLASSINFO("DefaultProperty", "data") \
+    public: \
+        QQmlListProperty<QObject> data() \
+        { \
+            return QQmlListProperty<QObject>(this, &m_objects); \
+        } \
+    private: \
+        QList<QObject *> m_objects; \
+    };
+
+#endif // LIRI_WAYLANDCLIENT_QML_UTILS_H

src/imports/waylandclient/plugin.cpp
@@ -33,6 +33,13 @@
 
 #include "wlrlayersurfacev1qml.h"
 #include "xdgshellqml.h"
+#include "utils.h"
+
+LIRI_DECLARE_CONTAINER_CLASS(WlrLayerShellV1)
+LIRI_DECLARE_CONTAINER_CLASS(WlrLayerSurfaceV1Qml)
+LIRI_DECLARE_CONTAINER_CLASS(XdgWmBase)
+LIRI_DECLARE_CONTAINER_CLASS(XdgSurfaceQml)
+LIRI_DECLARE_CONTAINER_CLASS(XdgPopupQml)
 
 class WaylandClientPlugin : public QQmlExtensionPlugin
 {
@@ -55,8 +62,8 @@ public:
         qmlRegisterUncreatableType<WlrExportDmabufFrameV1>(uri, versionMajor, versionMinor, "WlrExportDmabufFrameV1",
                                                            QStringLiteral("Cannot create a WlrExportDmabufFrameV1 instance"));
 
-        qmlRegisterType<WlrLayerShellV1>(uri, versionMajor, versionMinor, "WlrLayerShellV1");
-        qmlRegisterType<WlrLayerSurfaceV1Qml>(uri, versionMajor, versionMinor, "WlrLayerSurfaceV1");
+        qmlRegisterType<WlrLayerShellV1Container>(uri, versionMajor, versionMinor, "WlrLayerShellV1");
+        qmlRegisterType<WlrLayerSurfaceV1QmlContainer>(uri, versionMajor, versionMinor, "WlrLayerSurfaceV1");
 
         qmlRegisterType<WlrOutputManagerV1>(uri, versionMajor, versionMinor, "WlrOutputManagerV1");
         qmlRegisterUncreatableType<WlrOutputHeadV1>(uri, versionMajor, versionMinor, "WlrOutputHeadV1",
@@ -72,11 +79,11 @@ public:
         qmlRegisterUncreatableType<WlrScreencopyFrameV1>(uri, versionMajor, versionMinor, "WlrScreencopyFrameV1",
                                                          QStringLiteral("Cannot create a WlrScreencopyFrameV1 instance"));
 
-        qmlRegisterType<XdgWmBase>(uri, versionMajor, versionMinor, "XdgWmBase");
+        qmlRegisterType<XdgWmBaseContainer>(uri, versionMajor, versionMinor, "XdgWmBase");
         qmlRegisterType<XdgSurface>();
-        qmlRegisterType<XdgSurfaceQml>(uri, versionMajor, versionMinor, "XdgSurface");
+        qmlRegisterType<XdgSurfaceQmlContainer>(uri, versionMajor, versionMinor, "XdgSurface");
         qmlRegisterType<XdgPopup>();
-        qmlRegisterType<XdgPopupQml>(uri, versionMajor, versionMinor, "XdgPopup");
+        qmlRegisterType<XdgPopupQmlContainer>(uri, versionMajor, versionMinor, "XdgPopup");
     }
 };
 

commit 60b22961f8ae06b1b15eb675e01bb51f2fa68fc4

Add missing QQmlListProperty include
Fix build issues caused by 94fdc82b.

src/imports/waylandclient/utils.h
@@ -5,6 +5,8 @@
 #ifndef LIRI_WAYLANDCLIENT_QML_UTILS_H
 #define LIRI_WAYLANDCLIENT_QML_UTILS_H
 
+#include <QQmlListProperty>
+
 #define LIRI_DECLARE_CONTAINER_CLASS(className) \
     class className##Container : public className \
     { \